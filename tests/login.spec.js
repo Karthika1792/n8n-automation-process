3ed862c38fb631faa9ff3b9ef5f7c0bb8cb4d4b7
Karthika S <karthikaspanicker172@gmail.com>
2026-09-21 21:02:38 +0530

    Update login.spec.js with comment for email field test
    Added a comment for the positive case scenario in the email field test.

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -53,7 +53,7 @@ test.describe('Login Page Tests', () => {
 
     await expect(emailField).toHaveAttribute('type', 'email'); ////// Positive case and Enable it as developer fix as 3rd scenarior
     //await expect(emailField).toHaveAttribute('type', 'text'); ///// Negative case 
-   
+   // test
    
   });
 });
